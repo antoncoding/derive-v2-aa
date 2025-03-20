241774b2ddb8e4d4b1e823118dd8224d0b722f06
antoncoding <antonassocareer@gmail.com>
2025-03-20 22:42:10 +0800

    fix: informational fixes

--- src/intents/IntentExecutorBase.sol
+++ src/intents/IntentExecutorBase.sol
@@ -10,7 +10,7 @@ import {SafeERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/
  * @title  IntentExecutorBase
  * @notice A shared contract that allows authorized EOAs to execute intents
  */
-contract IntentExecutorBase is Ownable {
+abstract contract IntentExecutorBase is Ownable {
     using SafeERC20 for IERC20;
 
     /**
@@ -44,7 +44,7 @@ contract IntentExecutorBase is Ownable {
      * @param token The address of the token to rescue
      */
     function rescueToken(address token) external onlyOwner {
-        IERC20(token).safeTransfer(owner(), IERC20(token).balanceOf(address(this)));
+        IERC20(token).transfer(msg.sender, IERC20(token).balanceOf(address(this)));
     }
 
     /**
