d62d815ab51b15c69fe268d5c490cb8258b992f8
antoncoding <antonassocareer@gmail.com>
2025-04-08 17:37:36 +0800

    fix: correct fix of rescueToken

--- src/intents/IntentExecutorBase.sol
+++ src/intents/IntentExecutorBase.sol
@@ -44,7 +44,6 @@ abstract contract IntentExecutorBase is Ownable {
      * @param token The address of the token to rescue
      */
     function rescueToken(address token) external onlyOwner {
-
         // intentionally ignore return value
         address(token).call(abi.encodeWithSignature("transfer(address,uint256)", msg.sender, IERC20(token).balanceOf(address(this))));
     }
